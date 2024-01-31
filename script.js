b414d77b5c411ef6248a28be872119234bc4b964
aefortin <154523554+aefortin@users.noreply.github.com>
2024-01-31 16:20:24 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+require([
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/ImageryLayer",
+        "esri/layers/support/RasterFunction"
+      ], (Map, MapView, ImageryLayer, RasterFunction) => {
+        /***************************************
+         * Set up popup template of image layer
+         **************************************/
+
+        const imagePopupTemplate = {
+          // autocasts as new PopupTemplate()
+          title: "Data from {SensorName} satellite",
+          content: `
+            Rendered RGB values: <b>{Raster.ServicePixelValue} </b>
+            <br>Original values (B, G, R, NIR): <b>{Raster.ItemPixelValue} </b>
+            `
+        };
+
+        /*******************************************************************
+         * Create image layer with server defined raster function templates
+         ******************************************************************/
+
+        const serviceRFT = new RasterFunction({
+          functionName: "TorontoFalseColor",
+          variableName: "Raster"
+        });
+
+        const layer = new ImageryLayer({
+          url: "https://landsat2.arcgis.com/arcgis/rest/services/Landsat8_Views/ImageServer",
+          rasterFunction: "NDVI Colorized",
+          popupTemplate: imagePopupTemplate
+        });
+
+        /*************************
+         * Add image layer to map
+         ************************/
+
+        const map = new Map({
+          basemap: "hybrid",
+          layers: [layer]
+        });
+
+        const view = new MapView({
+          container: "viewDiv",
+          map: map,
+          center: {
+            // autocasts as esri/geometry/Point
+            x: -9972028.7047,
+            y: 4813697.1152,
+            spatialReference: 3857
+          },
+          zoom: 6,
+          popup: {
+            actions: []
+          }
+        });
+      });
